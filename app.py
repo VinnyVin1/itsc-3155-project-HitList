9775b9630253857f358b0bac7cfe2d87a592cafe
Evans <marvinevans2010@gmail.com>
2023-10-31 17:15:42 -0400

    Finished the Home page with its css. Added code to the signup.css file. Added code to the app.py file.

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.get('/')
-def index():
+def index_page():
     return render_template('index.html')
+
+@app.get
+
+@app.get('/signup_page')
+def signup_page():
+    return render_template('signup.html')
