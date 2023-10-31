81e2ce3359965b3699269dbc36da093b28a7e5f6
Zane Hutchens <zhut654@gmail.com>
2023-10-31 19:34:37 -0400

    Finished sign in page

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.get('/')
 def index():
     return render_template('index.html')
+
+@app.get('/sign_in_page')
+def sign_in_page():
+    return render_template('sign_in_page.html')
+
+@app.post('/sign_in_page')
+def sign_in():
+    # get data here when more functionality is established 
+    return redirect('index.html')
