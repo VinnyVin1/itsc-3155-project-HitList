bfaf42b3a5ec5b4fdc415e0c73c2dc401f9f4597
zhutchens <119628730+zhutchens@users.noreply.github.com>
2023-11-01 21:15:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,12 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.get('/')
-
 def index_page():
     return render_template('index.html')
-
-@app.get
 
 @app.get('/signup_page')
 def signup_page():
     return render_template('signup.html')
-def index():
-    return render_template('index.html')
 
 @app.get('/sign_in_page')
 def sign_in_page():
