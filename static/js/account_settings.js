932034929796fd7c8e47b60cbfbbe28b11e64dc9
Evans <marvinevans2010@gmail.com>
2023-12-13 22:27:00 -0500

    Modified a lot of files

--- static/js/account_settings.js
+++ static/js/account_settings.js
@@ -1,7 +1,9 @@
 'use strict'; 
 
+// shows and hides the 
 const buttons = document.querySelectorAll('.settings-btn'); 
 const divs = document.querySelectorAll('.prof-settings-form-group'); 
+const webpages = document.querySelectorAll('a');
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
@@ -12,4 +14,14 @@ for (let i = 0; i < buttons.length; i++) {
             divs[i].classList.add('hidden'); 
         }
     }); 
+}
+
+window.addEventListener('DOMContentLoaded', hidePage);
+
+function hidePage() {
+    webpages.forEach(page => {
+        if (page.classList.contains('hide')) {
+            page.classList.add('hidden');
+        }
+    });
 }
